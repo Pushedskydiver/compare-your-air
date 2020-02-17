78430f4ae587e466d10b8272d5c9b7ac9a78c8d3
Alex Clapperton <hi@alexclapperton.co.uk>
2020-02-17 07:32:58 +0000

    ✅ Update paragraph atom tests

--- src/components/01-Atoms/Paragraph/Paragraph.test.tsx
+++ src/components/01-Atoms/Paragraph/Paragraph.test.tsx
@@ -7,9 +7,17 @@ afterEach(() => {
 });
 
 describe('<Paragraph />', () => {
-  it('should match snapshot', () => {
+  it('should match default paragraph snapshot', () => {
     const { container } = render(
       <Paragraph>This is a p tag</Paragraph>
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should match snapshot of paragraph with no margin', () => {
+    const { container } = render(
+      <Paragraph noMargin>This is a p tag</Paragraph>
     );
 
     expect(container).toMatchSnapshot();
